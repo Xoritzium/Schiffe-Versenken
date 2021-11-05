4a66709b8409ee092ee7c4e7c32aba23d03acf71
Tom / Xori <tom.thiede@gmx.de>
2021-11-05 13:17:44 +0100

    test tset

--- Project-Abgabe 2. Semester - Schiffe versenken/src/SchiffeVersenkenTests/SchiffeVersenkenTest.java
+++ Project-Abgabe 2. Semester - Schiffe versenken/src/SchiffeVersenkenTests/SchiffeVersenkenTest.java
@@ -12,7 +12,7 @@ import junit.framework.Assert;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-// Test 
+// Test Test
 
 /*immer mindestens 5 oberer und unterer Rand testen (wenn nötig)
  * TESTS:
